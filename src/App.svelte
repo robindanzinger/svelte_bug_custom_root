b1707cbc3ca60f3c5c72f3b7901211ab78a258ba
Robin Danzinger <robin.danzinger@gmx.com>
2024-08-20 11:11:42 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="custom">this is custom, run dev server, changte the background-color to red or green.</div>
+<div class="custom">It should change background color, but it doesn't work on ubuntu</div>
+<div class="custom">Restart dev server. Now background-color is ok. Until you change the color again.</div>
+
+<style>
+.custom {
+  background-color: red;
+}
+
+</style>
